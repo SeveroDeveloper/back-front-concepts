a89b9cf7f1b86915eb0ec2ae0e6e640fa37825c0
Amanda Severo <amandasevero.work@gmail.com>
2022-05-19 00:16:24 -0300

    feat(integration): using axios to integrate with backend api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import Header from './components/Header';
 import './App.css';
@@ -7,28 +8,54 @@ import background from './assets/background.jpg'
 function App(){
 
   //useState returns a two positions array [the initial value of the variable, function to update the variable]
-  const [projects, setProjects] = useState(['New book', 'New draw']);
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject(){
+  // the first param is the function that will be triggered everytime a variable
+  // in the dependencies array (second param) changes
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject(){
     
-    setProjects([ ...projects, `One more added at ${Date.now()}`]);
+    /* This is a non api connected method
+    setProjects([ ...projects, 
+      {
+        "title" : `One more added at ${Date.now()}`,
+        "id" : `Id-${Date.now()}`,
+        "owner" : "whopress"
+      }
+    ]);
+    */
+
+    const response = await api.post('/projects', {
+      title: "New project added on api by POST",
+      onwer: "whoclick"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
 
     console.log(projects)
   }
 
   return (
     <>
+      <img src={background} width={200}></img>
       <Header title="Homepage">
         <ul>
           <li>First line</li>
           <li>Second line</li>
         </ul>
       </Header>
-      <img src={background} width={300}></img>
+      
       <Header title="Project"/>
 
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Add project</button>
